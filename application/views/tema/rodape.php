a1eb05e9ca16c5f805430beeb0c7bd8bf420c00b
giampaulo <48699495+giampaulo@users.noreply.github.com>
2020-05-16 10:13:53 -0300

    filemanager on mapos
    adicionado responsive file manager para gerenciar arquivos do mapos

---
+++ application/views/tema/rodape.php
@@ -0,0 +1,13 @@
+
+<!--Footer-part-->
+<div class="row-fluid">
+    <div id="footer" class="span12"> <a href="" target="_blank">
+        <?= date('Y'); ?> &copy; Map-OS</a></div>
+  </div>
+  <!--end-Footer-part-->
+  <script src="<?= base_url(); ?>assets/js/bootstrap.min.js"></script>
+  <script src="<?= base_url(); ?>assets/js/matrix.js"></script>
+</body>
+
+</html>
+
